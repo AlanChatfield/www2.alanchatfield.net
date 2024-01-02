bf69b13b4a0c33b67c7aa771ac08b5101239aed1
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2024-01-02 18:05:25 +0000

    Update createmdfilesfromghost.js

--- createmdfilesfromghost.js
+++ createmdfilesfromghost.js
@@ -84,7 +84,7 @@ const createMdFilesFromGhost = async () => {
         // Fetch  posts from the Ghost Content API
         const posts = await api.posts.browse({
             limit: 'all',
-            filter: 'tag:[politics,sport,hash-chatpress]',		
+            filter: 'tag:[hash-chatpress]',		
             include: 'tags,authors',
             formats: ['html'],
             order: 'published_at DESC'
